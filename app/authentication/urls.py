dc5d2478ecc3fbef29358d0b74c10aa785358481
Abdel Mejia <28445496+Bhalut@users.noreply.github.com>
2022-11-11 20:47:52 -0500

    Add Authentication App Django

---
+++ app/authentication/urls.py
@@ -0,0 +1,9 @@
+# from django.urls import re_path
+# #from authentication.views import RegisterView
+# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+#
+# urlpatterns = [
+#     # re_path(r'^token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+#     # re_path(r'^token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+#     # re_path(r'^register/', RegisterView.as_view(), name='auth_register'),
+# ]
